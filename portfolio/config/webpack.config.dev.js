68cfa5397127240de9a71eb3eb2aa06246e2e538
Austin <aleonardo2@ucmerced.edu>
2018-08-31 11:58:44 -0700

    fixed webpack.config.dev.js

--- portfolio/config/webpack.config.dev.js
+++ portfolio/config/webpack.config.dev.js
@@ -68,10 +68,7 @@ module.exports = {
       path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
   },
   resolve: {
-    moduleDirectories: [
-      'src',
-      'node_modules'
-    ]
+  
     // This allows you to set a fallback for where Webpack should look for modules.
     // We placed these paths second because we want `node_modules` to "win"
     // if there are any conflicts. This matches Node resolution mechanism.
