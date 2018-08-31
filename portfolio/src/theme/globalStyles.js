4ff3df72aa14dc2ac8e6ca5b27301b84f5cf06c6
Austin <aleonardo2@ucmerced.edu>
2018-08-31 10:43:50 -0700

    first commit

---
+++ portfolio/src/theme/globalStyles.js
@@ -0,0 +1,10 @@
+import { injectGlobal} from 'styled-components';
+/* eslint-disable*/
+injectGlobal`
+    @import url('https://fonts.googleapis.com/css?family=Inconsolata');
+
+    body {
+        font-family: 'Inconsolata', monospace;
+        font-size: 20px;
+    }
+`
